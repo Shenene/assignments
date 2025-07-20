43ba5e2b950aa58a922a8e9ccb0767dff4517be4
Shenene Carstens <shenene.carstens@gmail.com>
2025-07-20 13:28:35 +1200

    Added JS for my 'other' button on the 'report-lights' screen - 20/07/25 @ 1.28pm

--- Mission_4/sheneneMission04/script.js
+++ Mission_4/sheneneMission04/script.js
@@ -1,1 +1,28 @@
 "use strict";
+
+// Using the following classes/id's
+const buttons = document.querySelectorAll(".issue-options button");
+const otherBtn = document.getElementById("btn-other");
+const otherDesc = document.getElementById("other-desc");
+
+// Hide my 'other' box by default"
+otherDesc.style.display = "none";
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    // Remove highlight from all
+    buttons.forEach((b) => b.classList.remove("selected"));
+    // Add highlight to clicked
+    btn.classList.add("selected");
+
+    // Show input only if 'Other' is clicked
+    if (btn === otherBtn) {
+      otherDesc.style.display = "block";
+      // Move the focus to the input section
+      otherDesc.querySelector("input").focus();
+    } else {
+      otherDesc.style.display = "none";
+      otherDesc.querySelector("input").value = "";
+    }
+  });
+});
